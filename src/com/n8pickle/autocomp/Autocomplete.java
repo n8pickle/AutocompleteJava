b85db80d6879ff704eb4f5063ad1404eff3b27ca
n8pickle <n8pickle@gmail.com>
2019-10-17 14:48:32 -0600

    commit to clean directory and open new branch

--- src/com/n8pickle/autocomp/Autocomplete.java
+++ src/com/n8pickle/autocomp/Autocomplete.java
@@ -1,14 +1,17 @@
 package com.n8pickle.autocomp;
 
+import java.util.Arrays;
+
 public class Autocomplete {
+    private Term[] terms;
     // Initialize the data structure from the given array of terms.
     public Autocomplete(Term[] terms) {
-
+    this.terms = terms;
     }
 
     // Return all terms that start with the given prefix, in descending order of weight.
     public Term[] allMatches(String prefix) {
-
+        Arrays.sort(terms, Term.byPrefixOrder(prefix.length()));
     }
 
     // Return the number of terms that start with the given prefix.
