9184a6ad07987fa66f564c87da364dc9620d3414
n8pickle <n8pickle@gmail.com>
2019-10-18 15:10:05 -0600

    reimplemented binary search in the right way. Fixed Autocomplete.java, Added all items

--- src/com/n8pickle/autocomp/Autocomplete.java
+++ src/com/n8pickle/autocomp/Autocomplete.java
@@ -12,20 +12,13 @@ public class Autocomplete {
     // Return all terms that start with the given prefix, in descending order of weight.
     public Term[] allMatches(String prefix) {
         Arrays.sort(terms, Term.byPrefixOrder(prefix.length()));
-        int first = 0;
-        int last = terms.length - 1;
-        int middle;
-          while (first <= last)
-          {
-              middle = (first + last) / 2;
-              if(terms[middle].compareTo(new Term(prefix, 0))  == 0)
-                  return linearSearch(terms, middle, prefix);
-              else if (terms[middle].compareTo(new Term(prefix, 0)) < 0 )
-                  last = middle;
-              else
-                  first = middle;
-          }
-          return new Term[1];
+        int start = BinarySearchDeluxe.firstIndexOf(terms, new Term(prefix, 0),
+                Term.byPrefixOrder(prefix.length()));
+        int end = BinarySearchDeluxe.lastIndexOf(terms, new Term(prefix, 0),
+                Term.byPrefixOrder(prefix.length()));
+        Term[] result = Arrays.copyOfRange(terms, start, end);
+        Arrays.sort(terms, Term.byReverseWeightOrder());
+        return result;
     }
 
     // Return the number of terms that start with the given prefix.
@@ -33,37 +26,4 @@ public class Autocomplete {
             return allMatches(prefix).length;
     }
 
-    private Term[] linearSearch(Term[] middle, int mid, String prefix) {
-        boolean left = false;
-        boolean right = false;
-        int searchBreadth = middle.length / 2;
-        Term[] matches = new Term[10];
-        matches[0] = middle[mid];
-        int i = 1;
-        while (!left && searchBreadth >= i) {
-            if(middle[mid - i].compareTo(new Term(prefix, 0)) == 0) {
-                if(i + 1 == matches.length) {
-                    matches = Arrays.copyOf(matches, matches.length*2);
-                }
-                matches[i] = middle[mid - i];
-                i++;
-            } else {
-                left = true;
-            }
-        }
-        int j = 1;
-        while (!right && searchBreadth >= j) {
-            if(middle[mid - j].compareTo(new Term(prefix, 0)) == 0) {
-                if(i + j + 1 == matches.length) {
-                    matches = Arrays.copyOf(matches, matches.length*2);
-                }
-                matches[i + j] = middle[mid + j];
-                j++;
-            } else {
-                right = true;
-            }
-        }
-        Arrays.sort(matches, Term.byReverseWeightOrder());
-        return matches;
-    }
 }
--- src/com/n8pickle/autocomp/BinarySearchDeluxe.java
+++ src/com/n8pickle/autocomp/BinarySearchDeluxe.java
@@ -1,15 +1,66 @@
 package com.n8pickle.autocomp;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class BinarySearchDeluxe {
     // Return the index of the first key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        int first = 0;
+        int last = a.length - 1;
+        int index = -1;
+        int mid;
+
+        while(first < last) {
+            mid = ( first + last ) / 2;
+            if (comparator.compare(a[mid], key) == 0) {
+                index = mid;
+                last = mid - 1;
+            } else if (comparator.compare(a[mid], key) < 0) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+                System.out.println("smaller");
+            }
+        }
+        return index;
 
     }
 
     // Return the index of the last key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        int first = 0;
+        int last = a.length - 1;
+        int index = -1;
+        int mid;
+
+        while(first <= last) {
+            mid = ( first + last ) / 2;
+            if (comparator.compare(a[mid], key) == 0) {
+                index = mid;
+                first = mid + 1;
+            } else if (comparator.compare(a[mid], key) < 0) {
+                last = mid - 1;
+            } else {
+                first = mid + 1;
+            }
+        }
+        return index;
 
     }
+
+//    public static void main(String[] args) {
+//        Term[] terms = {new Term("abcd", 0), new Term("bbbb", 0),
+//                new Term("abca", 0), new Term("bbbb", 0), new Term("abcd", 0),
+//                new Term("cccc", 0), new Term("aaaa", 0),new Term("bbbb", 0),
+//                new Term("aaaa", 0), new Term("aaaa", 0), new Term("bbbb", 0)};
+//        Arrays.sort(terms, Term.byPrefixOrder(4));
+//        for (Term t: terms){
+//            System.out.println(t.toString());
+//        }
+//        int firstIndex = firstIndexOf(terms, new Term("dddd", 0), Term.byPrefixOrder(4));
+//        System.out.println(firstIndex);
+//        int lastIndex = lastIndexOf(terms, new Term("dddd", 0), Term.byPrefixOrder(4));
+//        System.out.println(lastIndex);
+//    }
 }
